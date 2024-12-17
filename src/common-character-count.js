b1bbc0515552ff5f115b3d2af7f073cbaa6c9498
kgbard <gkoja00@gmail.com>
2024-12-17 14:02:17 +0500

    C dating and CCC

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,21 +1,30 @@
-const { NotImplementedError } = require('../extensions/index.js');
+function getCommonCharacterCount(s1, s2) {
+    // Create character count maps for both strings
+    const charCount1 = {};
+    const charCount2 = {};
 
-/**
- * Given two strings, find the number of common characters between them.
- *
- * @param {String} s1
- * @param {String} s2
- * @return {Number}
- *
- * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
- * Strings have 3 common characters - 2 "a"s and 1 "c".
- */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+    // Count characters in first string
+    for (let char of s1) {
+        charCount1[char] = (charCount1[char] || 0) + 1;
+    }
+
+    // Count characters in second string
+    for (let char of s2) {
+        charCount2[char] = (charCount2[char] || 0) + 1;
+    }
+
+    // Calculate common character count
+    let commonCount = 0;
+    for (let char in charCount1) {
+        if (charCount2[char]) {
+            // Take the minimum count of the character between the two strings
+            commonCount += Math.min(charCount1[char], charCount2[char]);
+        }
+    }
+
+    return commonCount;
 }
 
 module.exports = {
-  getCommonCharacterCount
+    getCommonCharacterCount
 };
